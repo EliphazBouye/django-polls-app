0d8a759f1961e9fa82bee4ae8f1c58b7a2125ccc
Samuel Bouye <bouyesamuel@gmail.com>
2022-04-06 09:16:08 +0000

    make add new question and add related choice on same admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,12 @@
+from sre_constants import CH_LOCALE
 from django.contrib import admin
 
 from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -8,7 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Question details", {'fields': ['question_text']}),
         ("Date information", {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
