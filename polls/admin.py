02493b4401dd0dc48e2f41d1390db0393fd983b8
Samuel Bouye <bouyesamuel@gmail.com>
2022-04-06 09:31:31 +0000

    customiz question lists information

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -15,6 +15,10 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 50
 
 
 admin.site.register(Question, QuestionAdmin)
